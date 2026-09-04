da7fb3993d8e7e87f3efdd613e1b184d19d2014d
tom-zheng <flyingjulesfan@163.com>
2026-09-04 13:26:59 +0800

    set up environment, ran simple fastapi app on uvicorn

--- app/main.py
+++ app/main.py
@@ -2,6 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
+@app.get("/")   #When someone sends a GET request to the / path, call the function right below this line.
+def read_root(): 
     return {"message": "Hello, Farmware backend is running."}
