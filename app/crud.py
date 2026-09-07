b035a039e92360051dc26dcbaaea8bce62532243
tom-zheng <flyingjulesfan@163.com>
2026-09-07 15:11:34 +0800

    first build crud, with limited, basic functions

--- app/crud.py
+++ app/crud.py
@@ -22,3 +22,44 @@ def create_user(db: Session, user: schemas.UserCreate):
 def get_user(db:Session,user_id:int):
     # using sqlalchemy  query grammar
     return db.query(models.User).filter(models.User.id == user_id).first()
+
+
+# item template crud
+def create_item_template(db:Session, item_template: schemas.ItemTemplateCreate):
+    db_item_template = models.ItemTemplate(
+        name = item_template.name,
+        sku = item_template.sku,
+        description = item_template.description,
+        unit = item_template.unit
+    )
+    db.add(db_item_template)
+    db.commit()
+    # let db return id, etc...
+    db.refresh(db_item_template)
+    return db_item_template
+
+def get_item_template(db:Session, item_template_id:int):
+    return db.query(models.ItemTemplate).filter(models.ItemTemplate.id == item_template_id).first()
+
+
+# inventory item (inventory) crud
+def create_inventory(db:Session, inventory:schemas.InventoryCreate):
+    db_inventory = models.Inventory(
+        template_id = inventory.template_id,
+        quantity = inventory.quantity
+    )
+    db.add(db_inventory)
+    db.commit()
+    db.refresh(db_inventory)
+    return db_inventory
+
+def get_inventory(db:Session, inventory_id:int):
+    return db.query(models.Inventory).filter(models.Inventory.id == inventory_id).first()
+
+
+# logs crud (r only for read only)
+def get_item_template_log(db:Session,log_id:int):
+    return db.query(models.TemplateLog).filter(models.TemplateLog.id == log_id).first
+
+def get_inventory_log(db:Session,log_id:int):
+    return db.query(models.InventoryLog).filter(models.InventoryLog.id == log_id).first
